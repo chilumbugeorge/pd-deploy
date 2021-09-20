88a3db8d50aa2890d9de4ffdd34c3640187fcad2
George Chilumbu <chilumbu.george@gmail.com>
2021-09-20 22:49:39 +0800

    add 02_buyer.sql with changed table name

--- 02_buyer.sql
+++ 02_buyer.sql
@@ -1,3 +1,3 @@
-ALTER TABLE buyer_detail
+ALTER TABLE buyer_testing
 	ADD COLUMN `br_remark` text COLLATE utf8mb4_bin AFTER br_ccy,
 	ADD COLUMN `br_req_delete` tinyint(1) DEFAULT '0' AFTER br_status ;
