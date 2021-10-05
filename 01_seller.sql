74c1e91e7ea7b69a8084e2cb08f3b1a7aeb10192
George Chilumbu <chilumbu.george@gmail.com>
2021-10-05 20:52:51 +0800

    ShopAuction_QB-0.2.0_QS-0.8.2

--- 01_seller.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE seller_details
-	ADD COLUMN `sr_remark` text COLLATE utf8mb4_bin AFTER sr_type,
-	ADD COLUMN `sr_req_delete` tinyint(1) DEFAULT '0' AFTER sr_status ;
--- 02_buyer.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE buyer_testing
-	ADD COLUMN `br_remark` text COLLATE utf8mb4_bin AFTER br_ccy,
-	ADD COLUMN `br_req_delete` tinyint(1) DEFAULT '0' AFTER br_status ;
