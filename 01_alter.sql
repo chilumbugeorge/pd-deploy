a8368c2f6f6a38e86eafb1c8a97a638d8975b572
George Chilumbu <chilumbu.george@gmail.com>
2021-11-10 16:37:05 +0800

    version ShopAuction_QA-0.4.1

--- 01_alter.sql
+++ 01_alter.sql
@@ -1,7 +1,6 @@
 
 
-ALTER TABLE order_header 
-	ADD COLUMN `oh_buyer_remark` text COLLATE utf8mb4_bin DEFAULT NULL AFTER oh_status,
-	ADD COLUMN `oh_seller_remark` text COLLATE utf8mb4_bin DEFAULT NULL AFTER oh_buyer_remark;
+ALTER TABLE product_detail 
+	ADD COLUMN `pt_limit` int(10) unsigned NULL AFTER pt_quantity ;
 
   
