36572bd13cc2cc532afee2138afe914ac07692f9
George Chilumbu <chilumbu.george@gmail.com>
2021-12-22 20:10:14 +0800

    added ShopAuction_QA-0.6.0

--- 01_insert_data.sql
+++ 01_insert_data.sql
@@ -4,26 +4,18 @@
 INSERT INTO txn_supported 
 (ts_code, ts_desc, ts_channel_code, ts_support, ts_req_token, ts_req_email, ts_req_log, ts_disabled, ts_create_user, ts_create_timestamp, ts_update_user, ts_update_timestamp) 
 VALUES 
- ('AGOD', 'Admin Get Order Detail', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('AGCSL', 'Admin Get Cs Support List', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('AGCSD', 'Admin Get Cs Support Detail', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('ARCSD', 'Admin Reply Support Content', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('AGRDL', 'Admin Get Request Delete Listt', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()); 
+ ('BSE', 'Buyer Search Engine', 'API', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
+ ('AGRE', 'Admin Report Event', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
+ ('AGRP', 'Admin Report Product', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
+ ('AGRS', 'Admin Report Seller', 'ADM', '1', 'Y', 'N', 'Y', '0', 'SYSTEM', now(), 'SYSTEM', now()); 
 
-
-
-INSERT INTO def_cs_support_type 
-(dc_code, dc_desc, dc_disabled, dc_create_user, dc_create_timestamp, dc_update_user,dc_update_timestamp) 
-VALUES 
- ('ADMIN_REP', 'CS reply Contact', '0', 'SYSTEM', now(), 'SYSTEM', now()); 
 
  
  
- INSERT INTO def_err_code 
-(ec_code, ec_init_code, ec_desc, ec_disabled, ec_create_user, ec_create_timestamp, ec_update_user, ec_update_timestamp) 
-VALUES 
- ('90023', 'Get Seller Detail Fail', 'INT_INVALID_GET_SELLER_DETAIL_FAIL', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('90024', 'Get BuyerDetail Fail', 'INT_INVALID_GET_BUYER_DETAIL_FAIL', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('90025', 'Buyer ID Not Found', 'INT_INVALID_BUYER_ID', '0', 'SYSTEM', now(), 'SYSTEM', now()),   
- ('10200', 'Invalid Support ID', 'INT_INVALID_SUPPORT_ID', '0', 'SYSTEM', now(), 'SYSTEM', now()), 
- ('10201', 'Invalid Support Content', 'INT_INVALID_REPLY_CONTENT', '0', 'SYSTEM', now(), 'SYSTEM', now()); 
+INSERT INTO user_perm 
+(up_sect_id,up_perm_id,up_msg_code, up_cd, up_name, up_desc, up_disabled, up_create_user, up_create_timestamp, up_update_user, up_update_timestamp) 
+VALUES
+ ('800001','800001','AGRS','ADMIN_REPORT_SELLER','賣家銷售報表','查看賣家銷售報表','0','SYSTEM',now(),'SYSTEM',now()) , 
+ ('800001','800002','AGRP','ADMIN_REPORT_PRODUCT','產品銷售報表','查看產品銷售報表','0','SYSTEM',now(),'SYSTEM',now()) , 
+ ('800001','800003','AGRE','ADMIN_REPORT_EVENT','拍賣活動報表','查看拍賣活動報表','0','SYSTEM',now(),'SYSTEM',now()) ; 
+
